e9a7b1e7827712881b689cf00dcc4c3ea591ecc7
kyletower <kyle.tower@gmail.com>
2021-05-03 14:55:05 -0400

    hide form on submit, show loading gif

--- client/client.js
+++ client/client.js
@@ -1,6 +1,8 @@
 console.log("Hello, world!");
 
 const form = document.querySelector("form"); // whenever you see document, it's client/user side
+const loadingElement = document.querySelector(".loading");
+loadingElement.style.display = "none";
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,4 +15,7 @@ form.addEventListener("submit", (event) => {
     content,
   };
   console.log(mew);
+
+  form.style.display = "none";
+  loadingElement.style.display = "";
 });
