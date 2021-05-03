bdcfaa67454f70b47b2e27bcae837a8dfe66fd21
kyletower <kyle.tower@gmail.com>
2021-05-03 16:58:17 -0400

    npm installed cors, ensured server is receiving the data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors()); // adds cors as a middleware, all incoming requests passes through cors which adds the necessary headers to it.
+app.use(express.json()); // json body parser, any incoming req with json content type will be parsed by this middleware and put on the body.
 app.get("/", (reqest, response) => {
   response.json({
     message: "Meower! 🐈",
   });
+});
+
+app.post("/mews", (request, response) => {
+  console.log(request.body);
 });
 
 app.listen(5000, () => {
