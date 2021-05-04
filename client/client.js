bd6e0282a853a84bdf22a0b871e3e5371883ab4f
kyletower <kyle.tower@gmail.com>
2021-05-04 11:59:46 -0400

    Installed MongoDB. Log out created Mew after POST request

--- client/client.js
+++ client/client.js
@@ -3,6 +3,7 @@ console.log("Hello, world!");
 const form = document.querySelector("form"); // whenever you see document, it's client/user side
 const loadingElement = document.querySelector(".loading");
 loadingElement.style.display = "none";
+const API_URL = "http://localhost:5000/mews";
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,8 +15,20 @@ form.addEventListener("submit", (event) => {
     name,
     content,
   };
-  console.log(mew);
+  // console.log(mew);
 
   form.style.display = "none";
   loadingElement.style.display = "";
+
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(mew),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((createdMew) => {
+      console.log(createdMew);
+    });
 });
